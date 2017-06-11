bfabf020533304a510354ccdf33d928e7364c7a9
Andrew Kane <acekane1@gmail.com>
2017-06-11 11:46:16 -0700

    Better method for loading

--- lib/camo.rb
+++ lib/camo.rb
@@ -2,18 +2,21 @@ require "camo/version"
 require "openssl"
 
 module Camo
-
   def camo(image_url)
     hexdigest = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha1"), ENV["CAMO_KEY"], image_url)
     encoded_image_url = image_url.unpack('H*').first
     "#{ENV["CAMO_HOST"]}/#{hexdigest}/#{encoded_image_url}"
   end
-
 end
 
 if defined?(Rails)
-  ActionView::Base.send :include, Camo
-  ActionController::Base.send :include, Camo
+  ActiveSupport.on_load(:action_view) do
+    include Camo
+  end
+
+  ActiveSupport.on_load(:action_controller) do
+    include Camo
+  end
 end
 
 if defined?(Sinatra)
