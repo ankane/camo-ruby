2f50532d26f18886fe479abed3f65b98225b3ae4
Andrew <acekane1@gmail.com>
2017-11-18 16:43:50 -0800

    Raise error when no host

--- lib/camo.rb
+++ lib/camo.rb
@@ -4,6 +4,7 @@ require "openssl"
 module Camo
   def camo(image_url)
     raise "No CAMO_KEY" unless ENV["CAMO_KEY"]
+    raise "No CAMO_HOST" unless ENV["CAMO_HOST"]
     hexdigest = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new("sha1"), ENV["CAMO_KEY"], image_url)
     encoded_image_url = image_url.unpack('H*').first
     "#{ENV["CAMO_HOST"]}/#{hexdigest}/#{encoded_image_url}"
