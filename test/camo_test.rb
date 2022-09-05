94a39491d63a6d0fe46d26670b68226bbdfd71ab
Andrew Kane <andrew@ankane.org>
2022-09-05 00:09:58 -0700

    Improved tests

--- test/camo_test.rb
+++ test/camo_test.rb
@@ -1,8 +1,30 @@
 require_relative "test_helper"
 
 class CamoTest < Minitest::Test
-  def test_view_helper
-    expected = "http://camo.example.com/b82e650cfe20239b66f7165e54c3b9036d722aef/68747470733a2f2f7777772e6578616d706c652e636f6d2f706174682f746f2f696d6167652e706e67"
-    assert_equal expected, TestContainer.camo("https://www.example.com/path/to/image.png")
+  def test_png
+    url = camo("https://ankane.org/images/archer-large.png")
+    assert url.start_with?(ENV["CAMO_HOST"])
+    assert_equal "\x89PNG".b, URI.open(url).read[0..3]
+  end
+
+  def test_html
+    url = camo("https://ankane.org/")
+    assert_error url, 400, "Unsupported content-type returned"
+  end
+
+  def test_bad_signature
+    url = camo("https://ankane.org/images/archer-large.png")
+    assert_error url[0..-2], 403, "Bad Signature"
+  end
+
+  private
+
+  def assert_error(url, status, message)
+    error = assert_raises(OpenURI::HTTPError) do
+      URI.open(url)
+    end
+    response = error.io
+    assert_equal status, response.status[0].to_i
+    assert_equal "#{message}\n", response.string
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,10 +2,11 @@ require "bundler/setup"
 Bundler.require(:default)
 require "minitest/autorun"
 require "minitest/pride"
+require "open-uri"
 
-ENV["CAMO_HOST"] ||= "http://camo.example.com"
+ENV["CAMO_HOST"] ||= "http://localhost:8080"
 ENV["CAMO_KEY"]  ||= "461fbf74af826c3a1020"
 
-class TestContainer
-  extend Camo
+class Minitest::Test
+  include Camo
 end
