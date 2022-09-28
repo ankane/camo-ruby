d66d18096bbb25470fa0c1b4bb98f226afd7c4ec
Andrew Kane <andrew@ankane.org>
2022-09-28 11:35:47 -0700

    Added todo [skip ci]

--- lib/camo/helper.rb
+++ lib/camo/helper.rb
@@ -7,6 +7,7 @@ module Camo
       raise "No CAMO_KEY" unless key
       raise "No CAMO_HOST" unless host
 
+      # TODO support base64 encoding
       hexdigest = OpenSSL::HMAC.hexdigest("sha1", key, image_url)
       encoded_image_url = image_url.unpack1("H*")
       "#{host}/#{hexdigest}/#{encoded_image_url}"
